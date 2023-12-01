b1bab76d598d5cd17b1abb5a61bd4625b6088f20
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2023-12-01 17:01:51 +0200

    have some troubles with Slack page

--- src/route/index.js
+++ src/route/index.js
@@ -8,11 +8,13 @@ const router = express.Router()
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/', function (req, res) {
+router.get('/slack', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
+  res.render('slack', {
+    layout: null,
+  })
   //                  ↑↑ сюди вводимо JSON дані
 })
 
